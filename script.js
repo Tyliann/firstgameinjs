2902fe0ad7ec0024988c9fbc1e70876c327bec18
Tyliann <120262015+Tyliann@users.noreply.github.com>
2022-12-10 16:51:04 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+let yPosition = 0;
+let xPosition = 0;
+
+document.addEventListener("keydown", (event) => {
+  const div = document.getElementById("game")
+  if (event.key === "ArrowUp") {
+    if (yPosition > 0) {
+      yPosition--;
+      div.style.top = `${yPosition}px`
+    }
+  }
+
+  if (event.key === "ArrowDown") {
+    if (yPosition < 280) {
+      yPosition++;
+      div.style.top = `${yPosition}px`
+    }
+  }
+
+  if (event.key === "ArrowRight") {
+    if (xPosition < 780) {
+      xPosition++;
+      div.style.left = `${xPosition}px`
+    }
+
+  }
+
+  if (event.key === "ArrowLeft") {
+    if (xPosition > 0) {
+      xPosition--;
+      div.style.left = `${xPosition}px`
+    }
+  }
+
+
+})
